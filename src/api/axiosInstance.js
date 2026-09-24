5ffb6218a38b4f0463c35203d8fd6d8e642f39ab
niyasrahman1 <niyasrahmanki@gmail.com>
2026-09-24 11:41:53 +0530

    second  commit

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:4000",
+    baseURL: "https://rb-server-t3ev.onrender.com",
     timeout: 5000,
 })
 
